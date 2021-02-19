c48ef795c8b0a1ed052ad720bd41ff3b82713c20
abhijeetcb <65212150+abhijeetcb@users.noreply.github.com>
2021-02-19 19:31:08 +0530

    Update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -6,6 +6,7 @@ echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 
+
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
 echo 'The "npm start" command has a trailing ampersand so that the command runs'
@@ -15,7 +16,7 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
+sudo npm start &
 sleep 1
 echo $! > .pidfile
 set +x
